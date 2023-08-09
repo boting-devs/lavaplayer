17307be615f2880b6a5e652117a3b75345261515
Toπ <git@topi.wtf>
2023-08-09 19:49:00 +0200

    include response body in exception

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/tools/io/HttpClientTools.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/tools/io/HttpClientTools.java
@@ -147,7 +147,7 @@ public class HttpClientTools {
     int statusCode = response.getStatusLine().getStatusCode();
 
     if (!isSuccessWithContent(statusCode)) {
-      throw new IOException("Invalid status code for " + context + ": " + statusCode);
+      throw new IOException("Invalid status code for " + context + ": " + statusCode + " body: " + EntityUtils.toString(response.getEntity()));
     }
   }
 
